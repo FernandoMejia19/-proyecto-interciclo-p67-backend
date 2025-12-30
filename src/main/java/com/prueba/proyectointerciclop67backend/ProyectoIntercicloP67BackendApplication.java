a97221283ae3e64873e1585efe914429dcca2e4b
FernandoMejia19 <fercholkm19@gmail.com>
2025-12-30 15:38:32 -0500

    iniciando el backend

---
+++ src/main/java/com/prueba/proyectointerciclop67backend/ProyectoIntercicloP67BackendApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.proyectointerciclop67backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntercicloP67BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIntercicloP67BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prueba/proyectointerciclop67backend/ProyectoIntercicloP67BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.proyectointerciclop67backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntercicloP67BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
